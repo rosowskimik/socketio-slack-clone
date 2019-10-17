e03371296e341be0c61032b6e4cf2806ba78f0de
Mikołaj Rosowski <m.rosowski1@wp.pl>
2019-10-17 18:40:42 +0200

    Dynamically updating DOM based on namespace data

---
+++ public/scripts.js
@@ -0,0 +1,14 @@
+const socket = io('localhost:3000');
+
+socket.on('namespaceList', data => {
+  const namespacesDiv = document.querySelector('.namespaces');
+  data.forEach(namespace => {
+    namespacesDiv.innerHTML += `<div class="namespace" data-endpoint="${namespace.endpoint}"><img src="${namespace.img}" /></div>`;
+  });
+
+  document.querySelectorAll('.namespace').forEach(elem => {
+    elem.addEventListener('click', () =>
+      console.log(`${elem.dataset.endpoint}`)
+    );
+  });
+});
